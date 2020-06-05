c51576ec0900156146c483f9da4baae5b0801287
Marco Macchia <marcoooo_97@hotmail.it>
2020-06-05 17:39:48 +0200

    modifica

---
+++ Area triangolo.cpp
@@ -0,0 +1,19 @@
+//Macchia Marco 3A 09/10/2013
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+ //input
+ int b,h,p,a;
+ 
+ printf ("Digita il valore della base:");
+ scanf ("%d",&b);
+ printf ("Digita il valore dell'altezza:");
+ scanf ("%d",&h);   
+ p= b*h;
+ a= p/2;
+ //risultato
+ printf ("L'area e' %d\n",a);
+ 
+ system ("PAUSE");
+ return 0;
+}
